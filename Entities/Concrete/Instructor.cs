9446999b84f9c5ab57a5a3fef1cbfa210ab02314
SahraSinem <s.sinem_can@hotmail.com>
2023-12-12 00:51:45 +0300

    Instructor oluşturuldu

---
+++ Entities/Concrete/Instructor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Instructor
+    {
+        public int InstructorId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int EducationId { get; set; }
+    }
+}

fe069f5cd5bc576de60a243d77306c0f4ba728af
OnurBaha <koconurbaha@gmail.com>
2023-12-12 00:11:11 +0300

    Değişiklikler Kaydedildi.

---
+++ Entities/Concrete/Exam.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Exam
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public  int ExamDuration { get; set; }
+        public int NumberOfQuestion {  get; set; }
+        public int Point { get; set; }
+    }
+}

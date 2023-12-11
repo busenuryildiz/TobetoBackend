90b70907b5bced9253e53a7be9242724e1b26329
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-12-12 00:00:34 +0300

    Entities Oluşturuldu

---
+++ Entities/Concrete/Course.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class Course
+    {
+
+        public string CourseName { get; set; }
+        public int CourseDateId { get; set; }
+        public string ImagePath { get; set; }
+        public int CourseProgress { get; set; }
+        public float Point { get; set; }
+        public int Like { get; set; }
+        public bool Saveds { get; set; }
+    }
+}

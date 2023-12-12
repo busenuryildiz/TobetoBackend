41d12f3e0ada93255a14698795fe2afa9ee9cf4c
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-12-12 17:07:35 +0300

    Add Core

---
+++ Core/Entities/IEntityTimestamps.cs
@@ -0,0 +1,9 @@
+﻿namespace Core.Entities
+{
+    public interface IEntityTimestamps
+    {
+        DateTime CreatedDate { get; set; }
+        DateTime? UpdatedDate { get; set; }
+        DateTime? DeletedDate { get; set; }
+    }
+}

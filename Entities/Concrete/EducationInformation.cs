19f74a239f675d973d34351a294ed482b75050ac
Alican Dağıdır <alicandagidir@hotmail.com>
2023-12-12 15:37:29 +0300

    Entity added

---
+++ Entities/Concrete/EducationInformation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete
+{
+    public class EducationInformation
+    {
+        public string Status { get; set; }
+        public string University { get; set; }
+        public string Faculty { get; set; }
+        public DateTime BeginningYear { get; set; }
+        public DateTime? GraduationYear { get; set; }
+
+    }
+}

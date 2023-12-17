78b432f94069e14a2e096aac7e7332c8baac40ce
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-12-18 01:13:37 +0300

    Add Configuration

---
+++ DataAccess/Context/EntityConfigurations/RoleConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context.EntityConfigurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<Role>
+    {
+        public void Configure(EntityTypeBuilder<Role> builder)
+        {
+            builder.HasKey(r => r.Id);
+            builder.Property(r => r.Name).IsRequired().HasMaxLength(255);
+        }
+    }
+}
---
+++ DataAccess/Context/EntityConfigurations/StudentSkillConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using Entities.Concrete;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context.EntityConfigurations
+{
+    public class StudentSkillConfiguration : IEntityTypeConfiguration<StudentSkill>
+    {
+        public void Configure(EntityTypeBuilder<StudentSkill> builder)
+        {
+            builder.HasKey(ss => ss.Id);
+            builder.Property(ss => ss.StudentId).IsRequired();
+            builder.Property(ss => ss.SkillId).IsRequired();
+
+            builder.HasOne(ss => ss.Student)
+                .WithMany()
+                .HasForeignKey(ss => ss.StudentId);
+
+            builder.HasOne(ss => ss.Skill)
+                .WithMany()
+                .HasForeignKey(ss => ss.SkillId);
+        }
+    }
+}

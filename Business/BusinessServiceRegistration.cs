4cd1956a60c09f1f1f1ca8a5a10d740252c86a68
busenuryildiz <busenuryildiz5337@gmail.com>
2023-12-17 20:12:05 +0300

    Updated BusinessServiceRegistration

--- Business/BusinessServiceRegistration.cs
+++ Business/BusinessServiceRegistration.cs
@@ -11,19 +11,35 @@ using System.Threading.Tasks;
 
 namespace Business
 {
+
     public static class BusinessServiceRegistration
     {
         public static IServiceCollection AddBusinessServices(this IServiceCollection services)
         {
             services.AddScoped<ICourseService, CourseManager>();
             services.AddScoped<IUserService, UserManager>();
-            services.AddScoped<IExamService, ExamManager>();
-            services.AddScoped<INewService, NewManager>();
-            services.AddScoped<ICatalogService, CatalogManager>();
+
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+
+            services.AddSubClassesOfType(Assembly.GetExecutingAssembly(), typeof(BaseBusinessRules));
+            return services;
+        }
+        public static IServiceCollection AddSubClassesOfType(
+            this IServiceCollection services,
+            Assembly assembly,
+            Type type,
+            Func<IServiceCollection, Type, IServiceCollection>? addWithLifeCycle = null
+        )
+        {
+            var types = assembly.GetTypes().Where(t => t.IsSubclassOf(type) && type != t).ToList();
+            foreach (var item in types)
+                if (addWithLifeCycle == null)
+                    services.AddScoped(item);
 
                 else
                     addWithLifeCycle(services, type);
             return services;
         }
     }
+
 }

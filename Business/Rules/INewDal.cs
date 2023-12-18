a86575d689282c5cf8dc65a68ab109fa8ff6acfe
Alican Dağıdır <alicandagidir@hotmail.com>
2023-12-18 03:15:10 +0300

    CRUD Operations Instructor

--- Business/Rules/INewDal.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Business.Rules
-{
-    internal interface INewDal
-    {
-    }
-}
---
+++ Business/Rules/InstructorBusinessRules.cs
@@ -0,0 +1,18 @@
+﻿using Core.Business.Rules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Rules
+{
+    public class InstructorBusinessRules : BaseBusinessRules
+    {
+        //IInstructorDal _instructorDal;
+        //public InstructorBusinessRules(IInstructorDal instructorDal)
+        //{
+        //    _instructorDal = instructorDal;
+        //}
+    }
+}

5392b46bcafdd7b18e23e7dd17644881115cd90b
busenuryildiz <busenuryildiz5337@gmail.com>
2023-12-19 18:23:12 +0300

    Add Announcement CRUD operations and Request-Response classes

--- Business/Abstracts/IExamService.cs
+++ Business/Abstracts/IExamService.cs
@@ -4,16 +4,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Business.DTOs.Request.Exam;
+using Business.DTOs.Response.Exam;
 
 namespace Business.Abstracts
 {
-    //public interface IExamService
-    //{
-    //    Task<IPaginate<GetListExamResponse>> GetListAsync(PageRequest pageRequest);
-    //    Task<CreatedExamResponse> Add(CreateExamRequest createExamRequest);
-    //    Task<UpdatedExamResponse> Update(UpdateExamRequest updateExamRequest);
-    //    Task<DeletedExamResponse> Delete(DeleteExamRequest deleteExamRequest);
-    //    Task<CreatedExamResponse> GetById(Guid id);
+    public interface IExamService
+    {
+        Task<IPaginate<GetListExamResponse>> GetListAsync(PageRequest pageRequest);
+        Task<CreatedExamResponse> Add(CreateExamRequest createExamRequest);
+        Task<UpdatedExamResponse> Update(UpdateExamRequest updateExamRequest);
+        Task<DeletedExamResponse> Delete(DeleteExamRequest deleteExamRequest);
+        Task<CreatedExamResponse> GetById(int id);
 
-    //}
+    }
 }
---
+++ Business/DTOs/Response/Announcement/GetListAnnouncementResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Core.DataAccess.Paging;
+
+namespace Business.DTOs.Response.Announcement
+{
+    public class GetListAnnouncementResponse:BasePageableModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+    }
+}

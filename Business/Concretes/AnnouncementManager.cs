e10f3b9de5bf4f6c2f544d28dad1a0a50254e5e4
busenuryildiz <busenuryildiz5337@gmail.com>
2023-12-19 18:41:09 +0300

    Add AnnouncementsController and Manager

---
+++ Business/Concretes/AnnouncementManager.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using Business.Abstracts;
+using Business.DTOs.Request.Announcement;
+using Business.DTOs.Response.Announcement;
+using Business.Rules;
+using Core.DataAccess.Paging;
+using DataAccess.Abstracts;
+using Entities.Concretes;
+
+namespace Business.Concretes
+{
+    public class AnnouncementManager:IAnnouncementService
+    {
+        IAnnouncementDal _announcementDal;
+        IMapper _mapper;
+        AnnouncementBusinessRules _businessRules;
+
+        public AnnouncementManager(AnnouncementBusinessRules businessRules, IAnnouncementDal announcementDal, IMapper mapper)
+        {
+            _businessRules = businessRules;
+            _announcementDal = announcementDal;
+            _mapper = mapper;
+        }
+
+        public async Task<CreatedAnnouncementResponse> Add(CreateAnnouncementRequest createAnnouncementRequest)
+        {
+            Announcement announcement = _mapper.Map<Announcement>(createAnnouncementRequest);
+            Announcement createdAnnouncement = await _announcementDal.AddAsync(announcement);
+            CreatedAnnouncementResponse createdAnnouncementResponse = _mapper.Map<CreatedAnnouncementResponse>(createdAnnouncement);
+            return createdAnnouncementResponse;
+        }
+
+        public async Task<DeletedAnnouncementResponse> Delete(DeleteAnnouncementRequest deleteAnnouncementRequest)
+        {
+            var data = await _announcementDal.GetAsync(i => i.Id == deleteAnnouncementRequest.Id);
+            _mapper.Map(deleteAnnouncementRequest, data);
+            data.DeletedDate = DateTime.Now;
+            var result = await _announcementDal.DeleteAsync(data, true);
+            var result2 = _mapper.Map<DeletedAnnouncementResponse>(result);
+            return result2;
+        }
+
+        public async Task<CreatedAnnouncementResponse> GetById(int id)
+        {
+            var result = await _announcementDal.GetAsync(c => c.Id == id);
+            Announcement mappedAnnouncement = _mapper.Map<Announcement>(result);
+
+            CreatedAnnouncementResponse createdAnnouncementResponse = _mapper.Map<CreatedAnnouncementResponse>(mappedAnnouncement);
+
+            return createdAnnouncementResponse;
+        }
+
+        public async Task<IPaginate<GetListAnnouncementResponse>> GetListAsync(PageRequest pageRequest)
+        {
+            var data = await _announcementDal.GetListAsync(
+                index: pageRequest.PageIndex,
+                size: pageRequest.PageSize
+            );
+            var result = _mapper.Map<Paginate<GetListAnnouncementResponse>>(data);
+            return result;
+        }
+
+        public async Task<UpdatedAnnouncementResponse> Update(UpdateAnnouncementRequest updateAnnouncementRequest)
+        {
+            var data = await _announcementDal.GetAsync(i => i.Id == updateAnnouncementRequest.Id);
+            _mapper.Map(updateAnnouncementRequest, data);
+            data.UpdatedDate = DateTime.Now;
+            await _announcementDal.UpdateAsync(data);
+            var result = _mapper.Map<UpdatedAnnouncementResponse>(data);
+            return result;
+        }
+    }
+}
---
+++ WebAPI/Controllers/MediaPostsController.cs
@@ -0,0 +1,51 @@
+﻿using Business.Abstracts;
+using Business.DTOs.Request.MediaPost;
+using Core.DataAccess.Paging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MediaPostsController : ControllerBase
+    {
+        IMediaPostService _mediapostService;
+        public MediaPostsController(IMediaPostService mediapostService)
+        {
+            _mediapostService = mediapostService;
+        }
+        [HttpPost("AddMediaPost")]
+        public async Task<IActionResult> Add([FromBody] CreateMediaPostRequest createMediaPostRequest)
+        {
+            var result = await _mediapostService.Add(createMediaPostRequest);
+            return Ok(result);
+        }
+
+        [HttpDelete("DeleteMediaPost")]
+        public async Task<IActionResult> Delete([FromBody] DeleteMediaPostRequest deleteMediaPostRequest)
+        {
+            var result = await _mediapostService.Delete(deleteMediaPostRequest);
+            return Ok(result);
+        }
+        [HttpPut("UpdateMediaPost")]
+        public async Task<IActionResult> Update([FromBody] UpdateMediaPostRequest updateMediaPostRequest)
+        {
+            var result = await _mediapostService.Update(updateMediaPostRequest);
+            return Ok(result);
+        }
+
+        [HttpGet("GetListMediaPost")]
+        public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+        {
+            var result = await _mediapostService.GetListAsync(pageRequest);
+            return Ok(result);
+        }
+        [HttpGet("GetByIdMediaPost")]
+        public async Task<IActionResult> GetById([FromQuery] int id)
+        {
+            var result = await _mediapostService.GetById(id);
+            return Ok(result);
+        }
+    }
+}

3d42bfc8208bb9ce3d4b5803c5b8b898bc3d9e41
sevvalkaba0@gmail.com <sevvalkaba0@gmail.com>
2023-12-20 23:41:55 +0300

    Update CourseLevel

---
+++ DataAccess/Concretes/EfCourseLevelDal.cs
@@ -0,0 +1,21 @@
+﻿using Core.DataAccess.Repositories;
+using DataAccess.Abstracts;
+using DataAccess.Context;
+using Entities.Concretes;
+using Entities.Concretes.Courses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concretes
+{
+
+    public class EfCourseLevelDal : EfRepositoryBase<CourseLevel, int, TobetoContext>, ICourseLevelDal
+    {
+        public EfCourseLevelDal(TobetoContext context) : base(context)
+        {
+        }
+    }
+}

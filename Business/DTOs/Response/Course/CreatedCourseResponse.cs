f77c0c6b68828b83b806e95e4ea4e3ae008ddb96
OnurBaha <koconurbaha@gmail.com>
2023-12-20 18:05:39 +0300

    Course response and request updated

--- Business/DTOs/Response/Course/CreatedCourseResponse.cs
+++ Business/DTOs/Response/Course/CreatedCourseResponse.cs
@@ -9,9 +9,11 @@ namespace Business.DTOs.Response.Course
 {
     public class CreatedCourseResponse : BasePageableModel
     {
-        public int? Id { get; set; }
+        public int Id { get; set; }
         public int? CourseLevelId { get; set; }
+        public int? CourseStatusId { get; set; }
         public int? SoftwareLanguageId { get; set; }
+        public int? CourseSubjectId { get; set; }
         public int? CategoryId { get; set; }
         public string? Name { get; set; }
         public string? ImagePath { get; set; }

c44a1beb633633e93c343c096b22f025a11ed107
busenuryildiz <busenuryildiz5337@gmail.com>
2024-02-19 22:23:43 +0300

    Rename UpdatedSurveyAnswerResponse

--- Business/DTOs/Response/SurveyAnswer/UpdatedSurveyAnswerResponse.cs
+++ Business/DTOs/Response/SurveyAnswer/UpdatedSurveyAnswerResponse.cs
@@ -9,12 +9,10 @@ namespace Business.DTOs.Response.SurveyAnswer
 
     public class UpdatedSurveyAnswerResponse
     {
-        // Güncellenmiş bilgiler veya başka bir duruma uygun özellikler eklenebilir
         public int SurveyAnswerId { get; set; }
         public Guid UserID { get; set; }
         public int SurveyID { get; set; }
         public int QuestionID { get; set; }
         public string AnswerValue { get; set; }
-        // Diğer güncellenmiş özellikler eklenebilir
     }
 }

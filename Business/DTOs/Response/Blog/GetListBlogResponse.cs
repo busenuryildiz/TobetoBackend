26065fd906ffc1ce5dc2057f1e1b8fb45224a8d1
Alican Dağıdır <alicandagidir@hotmail.com>
2024-02-24 15:53:46 +0300

    Blog Update

--- Business/DTOs/Response/Blog/GetListBlogResponse.cs
+++ Business/DTOs/Response/Blog/GetListBlogResponse.cs
@@ -11,7 +11,6 @@ namespace Business.DTOs.Response.Blog
     {
         public int Id { get; set; }
         public string Title { get; set; }
-        public string Description { get; set; }
         public string Content { get; set; }
         public string ImagePath { get; set; }
         public DateTime CreatedDate { get; set; }
